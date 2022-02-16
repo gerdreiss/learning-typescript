27f316794e50412add52f2f7547fdad331e032d9
Gerd Reiss <gerd@reiss.pro>
2022-02-16 22:00:39 +0100

    [Typescript: The Complete Developer's Guide] types + error handling

--- udemy/complete-developers-guide/fetch-json/index.ts
+++ udemy/complete-developers-guide/fetch-json/index.ts
@@ -2,6 +2,29 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  const todo = response.data as Todo;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  --------------------------------------------------
+  TODO
+  --------------------------------------------------
+  ID        : ${id}
+  Title     : ${title}
+  Completed : ${completed}
+  --------------------------------------------------
+`);
+};
