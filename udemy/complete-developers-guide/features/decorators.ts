f8181fc1301ee790ee2325c64b84a805a925b9fa
Gerd Reiss <gerd@reiss.pro>
2022-03-04 14:19:51 +0100

    [Typescript: The Complete Developers's Guide] decorators

--- udemy/complete-developers-guide/features/decorators.ts
+++ udemy/complete-developers-guide/features/decorators.ts
@@ -1,19 +1,45 @@
+@classDec
 class Boat {
+  @propDec
   color: string = 'red';
 
+  @propDec
   get formattedColor(): string {
     return `This boat's color is ${this.color}`;
   }
 
-  @testDecorator
-  pilot(): void {
-    console.log('swish');
+  @logError('Oops, boat was sunk.\n')
+  pilot(@paramDec speed: string): void {
+    if (speed === 'fast') {
+      console.log('swish');
+    } else {
+      console.log('shoooo');
+    }
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log('Target: ', target);
-  console.log('Key: ', key);
+function classDec(constructor: typeof Boat) {
+  console.log(constructor);
 }
 
-testDecorator(Boat.prototype, 'pilot');
+function paramDec(target: any, key: string, index: number) {
+  console.log(key, index);
+}
+
+function propDec(target: any, key: string) {
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage, e);
+      }
+    };
+  };
+}
