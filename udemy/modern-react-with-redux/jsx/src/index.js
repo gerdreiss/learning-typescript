c63edc07fc176aaaf438e6770f91f0e106ae8eea
Gerd Reiss <gerd@reiss.pro>
2022-03-05 21:28:21 +0100

    [Modern React with Redux] JSX

--- udemy/modern-react-with-redux/jsx/src/index.js
+++ udemy/modern-react-with-redux/jsx/src/index.js
@@ -2,7 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const labelText = 'Enter name: ';
+  const buttonStyle = { backgroundColor: 'green', color: 'white' };
+  const buttonText = 'Submit';
+
+  return (
+    <div style={{ border: '1px solid green', paddingLeft: '10px' }}>
+      <label className="label" htmlFor="name">
+        {labelText}
+      </label>
+      <input id="name" type="text" />
+      <button style={buttonStyle}>{buttonText}</button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
