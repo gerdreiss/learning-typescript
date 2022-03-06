b58037c858686512afa63fba89bec0edd42806f4
Gerd Reiss <gerd@reiss.pro>
2022-03-06 15:36:30 +0100

    [Modern React with Redux] Component + Props

--- udemy/modern-react-with-redux/components/src/index.js
+++ udemy/modern-react-with-redux/components/src/index.js
@@ -5,12 +5,36 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author={faker.name.firstName()} text={faker.lorem.sentence()} />
-      <CommentDetail author={faker.name.firstName()} text={faker.lorem.sentence()} />
-      <CommentDetail author={faker.name.firstName()} text={faker.lorem.sentence()} />
-      <CommentDetail author={faker.name.firstName()} text={faker.lorem.sentence()} />
-      <CommentDetail author={faker.name.firstName()} text={faker.lorem.sentence()} />
-      <CommentDetail author={faker.name.firstName()} text={faker.lorem.sentence()} />
+      <CommentDetail
+        author={faker.name.firstName()}
+        time="Today at 1:45PM"
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        time="Today at 2:15PM"
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        time="Today at 3:05PM"
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        time="Yesterday at 3:40PM"
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        time="Last week Friday at 1:45PM"
+        text={faker.lorem.sentence()}
+      />
+      <CommentDetail
+        author={faker.name.firstName()}
+        time="Today at 4:15PM"
+        text={faker.lorem.sentence()}
+      />
     </div>
   );
 };
