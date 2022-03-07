a964e7dbe3d51bd6f0402b86c0ec80225753c4bc
Gerd Reiss <gerd@reiss.pro>
2022-03-07 21:17:20 +0100

    [Modern React with Redux] passing data and events between components

--- udemy/modern-react-with-redux/pics/src/components/App.js
+++ udemy/modern-react-with-redux/pics/src/components/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  onSearchSubmit(term) {
+    console.log(term);
+  }
   render() {
     return (
       <div className="ui container" style={{ marginTop: '50px' }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
