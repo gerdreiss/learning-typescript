6178c73fe3e9fb0723ee5c49f683210054012ecc
Gerd Reiss <gerd@reiss.pro>
2022-03-09 08:32:16 +0100

    [Modern React with Redux] Search: query wikipedia, display snippets

--- udemy/modern-react-with-redux/widgets/src/App.js
+++ udemy/modern-react-with-redux/widgets/src/App.js
@@ -1,4 +1,4 @@
-import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   { title: 'What is React?', content: 'React is a front end javascript framework' },
@@ -9,7 +9,7 @@ const items = [
 export default () => {
   return (
     <div>
-      <Accordion items={items} />
+      <Search />
     </div>
   );
 };
---
+++ udemy/modern-react-with-redux/widgets/src/components/Search.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Search = () => {
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term,
+        },
+      });
+
+      setResults(data.query.search);
+    };
+
+    if (term) {
+      search();
+    }
+  }, [term]);
+
+  const rendered = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <div className="description">
+            <span dangerouslySetInnerHTML={{ __html: result.snippet }} />
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input className="input" value={term} onChange={(e) => setTerm(e.target.value)} />
+        </div>
+      </div>
+      <div className="ui celled list">{rendered}</div>
+    </div>
+  );
+};
+
+export default Search;
