26374aec30868e3300bdca64580f42a9e8f81b82
Gerd Reiss <gerd@reiss.pro>
2022-03-15 09:16:40 +0100

    [Modern React with Redux] React with Redux

--- udemy/modern-react-with-redux/songs/src/components/App.js
+++ udemy/modern-react-with-redux/songs/src/components/App.js
@@ -1,8 +1,19 @@
+import { selectSong } from '../actions';
+import SongDetail from './SongDetail';
+import SongList from './SongList';
+
 const App = () => {
   return (
-    <center>
-      <h1>Hello World!</h1>{' '}
-    </center>
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
   );
 };
 
