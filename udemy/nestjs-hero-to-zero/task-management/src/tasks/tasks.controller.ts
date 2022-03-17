4b208378abc135a62e5b09a224975b4139f6c1d7
Gerd Reiss <gerd@reiss.pro>
2022-03-17 07:34:08 +0100

    [NestJS Zero to Hero] add more endpoints

--- udemy/nestjs-hero-to-zero/task-management/src/tasks/tasks.controller.ts
+++ udemy/nestjs-hero-to-zero/task-management/src/tasks/tasks.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
-import { Task } from './tasks.model';
+import { Task, TaskStatus } from './tasks.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -12,11 +21,39 @@ export class TasksController {
     return this.tasksService.getTasks();
   }
 
+  @Get('/:id')
+  getTask(@Param('id') id: string): Task {
+    return this.tasksService.getTask(id);
+  }
+
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Task {
     return this.tasksService.createTask(
       createTaskDto.title,
       createTaskDto.description,
     );
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    return this.tasksService.deleteTask(id);
+  }
+
+  @Put('/:id/start')
+  startTask(@Param('id') id: string): void {
+    return this.tasksService.startTask(id);
+  }
+
+  @Put('/:id/finish')
+  finishTask(@Param('id') id: string): void {
+    return this.tasksService.finishTask(id);
+  }
+
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): void {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
--- udemy/nestjs-hero-to-zero/task-management/src/tasks/tasks.service.ts
+++ udemy/nestjs-hero-to-zero/task-management/src/tasks/tasks.service.ts
@@ -10,6 +10,10 @@ export class TasksService {
     return this.tasks;
   }
 
+  getTask(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
   createTask(title: string, description: string): Task {
     const task: Task = {
       id: uuid(),
@@ -19,5 +23,30 @@ export class TasksService {
     };
     this.tasks.push(task);
     return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  startTask(id: string): void {
+    const task = this.getTask(id);
+    if (task) {
+      task.status = TaskStatus.IN_PROGRESS;
+    }
+  }
+
+  finishTask(id: string): void {
+    const task = this.getTask(id);
+    if (task) {
+      task.status = TaskStatus.DONE;
+    }
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): void {
+    const task = this.getTask(id);
+    if (task) {
+      task.status = status;
+    }
   }
 }
