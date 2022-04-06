55ee53dd9c632ef4e618b971a4ac72940ae90ddb
Gerd Reiss <gerd@reiss.pro>
2022-04-06 20:09:07 +0200

    [Angular - The Complete Guild] First app - property binding vs string interpolation

--- udemy/angular-the-complete-guide/my-first-app/src/app/servers/servers.component.ts
+++ udemy/angular-the-complete-guide/my-first-app/src/app/servers/servers.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   // selector: '[app-servers]', <-- select by attribute
-  selector: '.app-servers', // <-- select by class
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // selector: '.app-servers',  <-- select by class
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit(): void {}
 }
