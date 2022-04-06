f49ac517c5f68031805bb7ef10656aefc4457a20
Gerd Reiss <gerd@reiss.pro>
2022-04-06 18:58:31 +0200

    [Angular - The Complete Guild] First app - components

--- udemy/angular-the-complete-guide/my-first-app/src/app/app.module.ts
+++ udemy/angular-the-complete-guide/my-first-app/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessComponent } from './success/success.component';
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningComponent,
+    SuccessComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ udemy/angular-the-complete-guide/my-first-app/src/app/success/success.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success',
+  template: `<p>Success!</p>`,
+  styles: [
+    `
+      p {
+        color: green;
+      }
+    `,
+  ],
+})
+export class SuccessComponent {}
